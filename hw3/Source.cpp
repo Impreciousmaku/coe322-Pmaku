6507805ea1f2debd34d52a949e1a6790b3ef03a0
Impreciousmaku <143474216+Impreciousmaku@users.noreply.github.com>
2023-11-04 02:37:23 -0500

    Add files via upload

---
+++ hw3/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+// Precious Maku oem354
+void splitOddEven(const std::vector<int>& input, std::vector<int>& odd, std::vector<int>& even) {
+    for (int num : input) {
+        if (num % 2 == 0) {
+            // If the number is even, add it to the 'even' vector
+            even.push_back(num);
+        }
+        else {
+            // If the number is odd, add it to the 'odd' vector
+            odd.push_back(num);
+        }
+    }
+}
+
+int main() {
+    // Input 
+    std::vector<int> input = { 5, 6, 2, 4, 5 };
+
+    std::vector<int> oddNumbers;
+    std::vector<int> evenNumbers;
+
+    splitOddEven(input, oddNumbers, evenNumbers);
+
+    // show odd and even vectors
+    std::cout << "Odd Numbers: ";
+    for (int num : oddNumbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Even Numbers: ";
+    for (int num : evenNumbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
